42e072d469d4fa3d213dae62b3f87e94c9e835fa
Jessica C <j.conrad@interface3.be>
2024-10-21 16:40:00 +0200

    end of day 21 oct

--- Exos/Exos lunch_14Oct2024- table, array, list dictionary/Program.cs
+++ Exos/Exos lunch_14Oct2024- table, array, list dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Xml.Serialization;
 using static System.Formats.Asn1.AsnWriter;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -81,75 +82,200 @@ namespace Exos_lunch_14Oct2024__table__array__list_dictionary
 
 
 
-            //4.Créez une application Console permettant de gérer une To-Do List(liste de choses à faire).
-            //L’utilisateur doit pouvoir introduire une tâches, supprimer une tâche et modifier une tâche.
+            ////4.Créez une application Console permettant de gérer une To-Do List(liste de choses à faire).
+            ////L’utilisateur doit pouvoir introduire une tâche, supprimer une tâche et modifier une tâche.
 
-            List<string> todoList = new List<string>();
-            Console.Write("Welcome to your personal to-do list!  To add an item, enter A.  To delete an item, enter D.  To modify an item, enter M.  To quit, enter Q.");
-            string choice = Console.ReadLine().ToUpper();
+            //List<string> todoList = new List<string>();
+            //Console.Write("Welcome to your personal to-do list!  To add an item, enter A.  To delete an item, enter D.  To modify an item, enter M.  To quit, enter Q.");
+            //string choice = Console.ReadLine().ToUpper();
 
-            while (choice != "Q")
+            //while (choice != "Q")
+            //{
+
+            //    switch (choice)
+            //    {
+
+            //        case "D":
+            //            if (todoList.Count == 0)
+            //            {
+            //                Console.WriteLine("There are no items to delete!");
+            //                choice = "";
+            //                break;
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Please type the number of the item to delete");
+            //                string deleteString = Console.ReadLine();
+            //                bool deleteStringOk = int.TryParse(deleteString, out int deleteChoice);
+            //                if (deleteStringOk && deleteChoice > 0 && deleteChoice <= todoList.Count)
+            //                {
+            //                    deleteChoice = deleteChoice - 1;
+            //                    todoList.RemoveAt(deleteChoice);
+            //                    choice = "";
+            //                    break;
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("invalid choice, you must enter a number that corresponds with a list item");
+            //                    choice = "";
+            //                }
+
+            //            }
+            //            break;
+
+            //        case "M":
+            //            if (todoList.Count == 0)
+            //            {
+            //                Console.WriteLine("There are no items to modify!");
+            //                choice = "";
+            //            }
+            //            else
+            //            {
+            //                Console.WriteLine("Please type the number of the item to modify:");
+            //                string modifString = Console.ReadLine();
+            //                bool modifStringOk = int.TryParse(modifString, out int modifChoice);
+            //                if (modifStringOk && modifChoice > 0 && modifChoice <= todoList.Count)
+            //                {
+            //                    modifChoice = modifChoice - 1;
+            //                    Console.WriteLine("Please type what you would prefer this item show");
+            //                    string modifEntry = Console.ReadLine();
+            //                    todoList[modifChoice] = modifEntry;
+            //                    choice = "";
+            //                }
+            //                else
+            //                {
+            //                    Console.WriteLine("invalid choice, you must enter a number that corresponds with a list item");
+            //                    choice = "";
+            //                }
+            //            }
+            //            break;
+
+
+            //        case "A":
+            //            Console.WriteLine("Please enter the new item :");
+            //            todoList.Add(Console.ReadLine());
+            //            choice = "";
+            //            break;
+
+            //        case "":
+            //            Console.Write("To add an item, enter A.  To delete an item, enter D.  To modify an item, enter M.  To quit, enter Q.");
+            //            choice = Console.ReadLine().ToUpper(); ;
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Invalid option. To add an item, enter A.  To delete an item, enter D.  To modify an item, enter M.  To quit, enter Q.");
+            //            choice = "";
+            //            choice = Console.ReadLine().ToUpper();
+            //            break;
+
+            //    }
+
+            //    if (todoList.Count > 0)
+            //    {
+            //        for (int i = 0; i < todoList.Count; i++)
+            //        {
+            //            Console.WriteLine($"item {i + 1}: {todoList[i]}");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Your to-do list is currently empty.");
+            //        choice = "";
+            //        break;
+            //    }
+            //}
+            //Console.WriteLine("Thanks for using your personal to-do list, and goodbye!");
+
+
+
+            //5.Réalisez une application Console nous permettant de déplacer un pion dans un tableau de 10
+            //éléments.Au début, le pion se trouve dans la première case du tableau. Nous pouvons ensuite
+            //le déplacer par la gauche(g), par la droite(d) ou de stopper l'algorithme (q).
+
+
+            //string[] game = { " x ", " 0 ", " 0 ", " 0 ", " 0 ", " 0 ", " 0 ", " 0 ", " 0 ", " 0 " };
+            //string userInput;
+            //int positionPawn = 0;
+            //do
+            //{
+
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.Write(game[i]);
+            //    }
+            //    Console.WriteLine("Please indicate in which direction you'd like to move the pawn (the x).  Type L for left, R for right and Q to quit this program.");
+
+            //    userInput = Console.ReadLine().ToUpper();
+            //    if (userInput == "L" && positionPawn == 0)
+            //    {
+            //        Console.WriteLine("Sorry, the pawn is already as far left as it can go!");
+            //    }
+            //    else if (userInput == "L" && positionPawn != 0)
+            //    {
+            //        game[positionPawn] = " 0 ";
+            //        positionPawn--;
+            //        game[positionPawn] = " x ";
+            //    }
+            //    if (userInput == "R" && positionPawn == 9)
+            //    {
+            //        Console.WriteLine("Sorry, the pawn is already as far right as it can go!");
+            //    }
+            //    else if (userInput == "R" && positionPawn != 9)
+            //    {
+            //        game[positionPawn] = " 0 ";
+            //        positionPawn++;
+            //        game[positionPawn] = " x ";
+            //    }
+
+
+            //} while (userInput != "Q");
+            //if (userInput == "Q")
+            //{
+            //    Console.WriteLine("Thanks for playing with the pawn!  Byeeeeee.");
+            //}
+
+
+            //6.A l’aide d’un Dictionary, créez un jeu du « Pierre - Papier - Ciseaux » (utilisez le Random pour
+            //faire choisir l’ordinateur) 
+
+            Dictionary<int, string> choices = new Dictionary<int, string>();
+            choices.Add(1, "rock");
+            choices.Add(2, "paper");
+            choices.Add(3, "scissors");
+
+            Random rnd = new Random();
+            int ordiChoice = rnd.Next(1, 4);
+            int userChoice;
+            //Console.WriteLine(ordiChoice);
+            do
             {
-
-                switch (choice)
+                ordiChoice = rnd.Next(1, 4);
+                Console.WriteLine(ordiChoice);
+                Console.WriteLine("Enter one of the following: 1 (for ROCK), 2 (for PAPER), or 3 (for SCISSORS).  Enter 4 to quit.");
+                bool userChoiceParsable = int.TryParse(Console.ReadLine(), out userChoice);
+                if (userChoiceParsable && userChoice != 4)
                 {
-                    case "A":
-                        Console.WriteLine("Please enter the new item :");
-                        todoList.Add(Console.ReadLine());
-                        choice = ""; //provokes infinite item 1 added!
-                        break;
-
-                    case "D":
-                        if (todoList.Count == 0)
-                        {
-                            Console.WriteLine("There are no items to delete!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please type the number of the item to delete");
-                            int deleteChoice = int.Parse(Console.ReadLine()) - 1;
-                            todoList.RemoveAt(deleteChoice);
-                        }
-                        break;
-
-                    case "M":
-                        if (todoList.Count == 0)
-                        {
-                            Console.WriteLine("There are no items to modify!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Please type the number of the item to modify:");
-                            int modifyChoice = int.Parse(Console.ReadLine()) - 1;
-                            Console.WriteLine("Please type what you would prefer this item show");
-                            string modifEntry = Console.ReadLine();
-                            todoList[modifyChoice] = modifEntry;
-                        }
-                        break;
-
-
-                }
-                if (todoList.Count > 0)
-                {
-                    for (int i = 0; i < todoList.Count; i++)
+                    Console.WriteLine($"I chose {choices[ordiChoice]}");
+                    Console.WriteLine($"you chose {choices[userChoice]}");
+                    if ((ordiChoice == 1 && userChoice == 3) || (ordiChoice == 2 && userChoice == 1) || (ordiChoice == 3 && userChoice == 2))
                     {
-                        Console.WriteLine($"item {i + 1}: {todoList[i]}");
+                        Console.WriteLine("I win!");
+                    }
+                    else if (ordiChoice == userChoice)
+                    {
+                        Console.WriteLine("It's a tie");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You won!");
                     }
                 }
-                else
+                else if (userChoice != 4)
                 {
-                    Console.WriteLine("Your to-do list is currently empty.");
+                    Console.WriteLine("You must enter either 1 (for ROCK), 2 (for PAPER), or 3 (for SCISSORS).");
                 }
-
-
-
-            }
-
-            Console.WriteLine("Thanks for using your personal to-do list, and goodbye!");
-
-
-
-
+            } while (userChoice != 4);
+            Console.WriteLine("Thanks for playing!");
 
 
 
@@ -158,5 +284,5 @@ namespace Exos_lunch_14Oct2024__table__array__list_dictionary
 
 
         }
-    }
+    }   
 }
---
+++ I3-WAD24-Base-Demos/Demo 13/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Demo_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menuOptions = { "Female", "Male", "Other" };
+            int selectedPosition = 0;
+            //string userChoice;
+            ConsoleKey userChoice;
+
+            do
+            {
+                Console.Clear(); //gives the illusion of deplacing the choice rather than regenerating everything and showing the new choice
+                Console.WriteLine("Please indicate your gender: ");
+                for (int i = 0; i < menuOptions.Length; i++)
+                {
+                    string option = menuOptions[i];
+                    if (i == selectedPosition)
+                    {
+                        Console.WriteLine($"[{option}]");
+                    }
+                    else
+                    {
+                        Console.WriteLine(option);
+                    }
+                }
+                Console.WriteLine("Type (U)p or (D)own or (S)ubmit");
+                //userChoice = Console.ReadLine().ToUpper();
+                userChoice = Console.ReadKey(true).Key;//the (true) intercepts the key without displaying the hit key onscreen.
+                //if (userChoice == "U" && selectedPosition > 0)
+                if (userChoice == ConsoleKey.U && selectedPosition > 0)
+                {
+                    selectedPosition--;
+                }
+                //else if (userChoice == "D" && selectedPosition < menuOptions.Length - 1)
+                else if (userChoice == ConsoleKey.D && selectedPosition < menuOptions.Length - 1)
+                {
+                    selectedPosition++;
+                }
+            }
+            //while (userChoice != "S");
+            while (userChoice != ConsoleKey.S);
+
+            Console.WriteLine($"You have chosen {menuOptions[selectedPosition]}");
+
+
+
+
+
+        }
+    }
+}
