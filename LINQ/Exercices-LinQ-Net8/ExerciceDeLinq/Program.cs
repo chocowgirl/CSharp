802316b41bc5d961368ac3693468bb9456acfc28
JC <jessica.l.conrad@gmail.com>
2025-01-13 16:57:44 +0100

    Samuels LINQ stuff

---
+++ LINQ/Exercices-LinQ-Net8/ExerciceDeLinq/Program.cs
@@ -0,0 +1,30 @@
+﻿using LINQDataContext;
+
+namespace ExerciceDeLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataContext dc = new DataContext();
+
+            //Faites vos exos ici
+            //2.1 --ecrire une requete pour presenter pour chaque étudiant le nom, la date de naissance, le login et le resultat pour l'année.
+
+            var studNomDBLoginResult = dc.Students.Select((s) => new { s.Last_Name, s.BirthDate, s.Login, s.Year_Result });
+
+            foreach var item in studNomDBLoginResult) { 
+                
+            }
+
+
+
+
+
+
+            #region Console.ReadLine()
+            Console.ReadLine();
+            #endregion
+        }
+    }
+}
