4cd2e95b68b2cd729c28e07f0f7464bc347cc2e7
Jessica C <j.conrad@interface3.be>
2025-01-20 16:03:32 +0100

    all the ASP.Net stuff from last friday

---
+++ DemosASP1/Demo-ASP1/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_ASP1.Controllers
+{
+    public class DemoController : Controller
+    {
+
+        [ViewData]
+        public string Title { get; set; }
+        public IActionResult Index()
+        {
+            //Here the view doesn't correspond to the name of the action
+            //return View("IndexOld");
+            Title = "Acceuil";
+            return View();
+        }
+
+        [Route("Demo/Table/{nb}")]
+        public IActionResult Table(int nb)
+        {
+            ViewData["table"] =nb; //contains the information of the variable table in Table.cshtml
+            return View();
+        }
+
+
+    }
+}
---
+++ ExosASP/MiniExoASP17Jan2025/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiniExoASP17Jan2025.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Register");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+    }
+}
